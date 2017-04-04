8cfc2337ce89e7ffc39446b1ec0a9dc6aa0b5cb4
Tobias Wilchen <tobias@wilchen.dk>
2017-04-04 14:20:26 +0200

    use JavaScript Standard Style

--- all-colors/allcolors.js
+++ all-colors/allcolors.js
@@ -2,113 +2,112 @@
 // https://github.com/bgrins/devtools-snippets
 // Print out CSS colors used in elements on the page.
 // Modifications by Tobias Wilchen (https://github.com/wilchen558)
+/* global chrome */
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        if (request.settings) {
-            execAllCorlors(request.settings)
-        } else {
-            console.log('Something went wrong');
+      if (request.settings) {
+        execAllCorlors(request.settings)
+      } else {
+        console.log('Something went wrong')
+      }
+    })
+
+function execAllCorlors (settings) {
+  let allColors = {}
+  let logToConsole = settings.logToConsole
+  let includeBorderColorsWithZeroWidth = settings.includeBorderColorsWithZeroWidth
+  let props = settings.props
+  let skipColors = settings.skipColors;
+
+  [].forEach.call(document.querySelectorAll('*'), function (node) {
+    let nodeColors = {}
+    props.forEach(function (prop) {
+      let color = window.getComputedStyle(node, null).getPropertyValue(prop)
+      let thisIsABorderProperty = (prop.indexOf('border') !== -1)
+      let notBorderZero = thisIsABorderProperty ? window.getComputedStyle(node, null).getPropertyValue(prop.replace('color', 'width')) !== '0px' : true
+      let colorConditionsMet
+
+      if (includeBorderColorsWithZeroWidth) {
+        colorConditionsMet = color && !skipColors[color]
+      } else {
+        colorConditionsMet = color && !skipColors[color] && notBorderZero
+      }
+
+      if (colorConditionsMet) {
+        if (!allColors[color]) {
+          allColors[color] = {
+            count: 0,
+            nodes: []
+          }
         }
-    });
 
-function execAllCorlors(settings) {
+        if (!nodeColors[color]) {
+          allColors[color].count++
+          allColors[color].nodes.push(node)
+        }
 
-    let allColors = {};
-    let logToConsole = settings.logToConsole;
-    let includeBorderColorsWithZeroWidth = settings.includeBorderColorsWithZeroWidth;
-    let props = settings.props;
-    let skipColors = settings.skipColors;
+        nodeColors[color] = true
+      }
+    })
+  })
 
-    [].forEach.call(document.querySelectorAll("*"), function (node) {
-        let nodeColors = {};
-        props.forEach(function (prop) {
-            let color = window.getComputedStyle(node, null).getPropertyValue(prop),
-                thisIsABorderProperty = (prop.indexOf("border") != -1),
-                notBorderZero = thisIsABorderProperty ? window.getComputedStyle(node, null).getPropertyValue(prop.replace("color", "width")) !== "0px" : true,
-                colorConditionsMet;
+  function rgbTextToRgbArray (rgbText) {
+    return rgbText.replace(/\s/g, '').match(/\d+,\d+,\d+/)[0].split(',').map(function (num) {
+      return parseInt(num, 10)
+    })
+  }
 
-            if (includeBorderColorsWithZeroWidth) {
-                colorConditionsMet = color && !skipColors[color];
-            } else {
-                colorConditionsMet = color && !skipColors[color] && notBorderZero;
-            }
+  function componentToHex (c) {
+    let hex = c.toString(16)
+    return hex.length === 1 ? '0' + hex : hex
+  }
 
-            if (colorConditionsMet) {
-                if (!allColors[color]) {
-                    allColors[color] = {
-                        count: 0,
-                        nodes: []
-                    };
-                }
+  function rgbToHex (rgbArray) {
+    let r = rgbArray[0]
+    let g = rgbArray[1]
+    let b = rgbArray[2]
+    return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b)
+  }
 
-                if (!nodeColors[color]) {
-                    allColors[color].count++;
-                    allColors[color].nodes.push(node);
-                }
+  let allColorsSorted = []
+  for (let i in allColors) {
+    let rgbArray = rgbTextToRgbArray(i)
+    let hexValue = rgbToHex(rgbArray)
 
-                nodeColors[color] = true;
-            }
-        });
-    });
+    allColorsSorted.push({
+      key: i,
+      value: allColors[i],
+      hexValue: hexValue
+    })
+  }
 
-    function rgbTextToRgbArray(rgbText) {
-        return rgbText.replace(/\s/g, "").match(/\d+,\d+,\d+/)[0].split(",").map(function (num) {
-            return parseInt(num, 10);
-        });
-    }
+  allColorsSorted = allColorsSorted.sort(function (a, b) {
+    return b.value.count - a.value.count
+  })
 
-    function componentToHex(c) {
-        let hex = c.toString(16);
-        return hex.length == 1 ? "0" + hex : hex;
-    }
+  let nameStyle = 'font-weight:normal;'
+  let countStyle = 'font-weight:bold;'
+  function colorStyle (color) {
+    return 'background:' + color + ';color:' + color + ';border:1px solid #333;'
+  };
 
-    function rgbToHex(rgbArray) {
-        let r = rgbArray[0],
-            g = rgbArray[1],
-            b = rgbArray[2];
-        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-    }
+  if (logToConsole) {
+    console.group('Total colors used in elements on the page: ' + window.location.href + ' are ' + allColorsSorted.length)
+    allColorsSorted.forEach(function (c) {
+      console.groupCollapsed('%c    %c ' + c.key + ' ' + c.hexValue + ' %c(' + c.value.count + ' times)',
+                colorStyle(c.key), nameStyle, countStyle)
+      c.value.nodes.forEach(function (node) {
+        console.log(node)
+      })
+      console.groupEnd()
+    })
+    console.groupEnd('All colors used in elements on the page')
+  }
 
-    let allColorsSorted = [];
-    for (let i in allColors) {
-        let rgbArray = rgbTextToRgbArray(i);
-        let hexValue = rgbToHex(rgbArray);
-
-        allColorsSorted.push({
-            key: i,
-            value: allColors[i],
-            hexValue: hexValue
-        });
-    }
-
-    allColorsSorted = allColorsSorted.sort(function (a, b) {
-        return b.value.count - a.value.count;
-    });
-
-    let nameStyle = "font-weight:normal;";
-    let countStyle = "font-weight:bold;";
-    function colorStyle(color) {
-        return "background:" + color + ";color:" + color + ";border:1px solid #333;";
-    };
-
-    if (logToConsole) {
-        console.group("Total colors used in elements on the page: " + window.location.href + " are " + allColorsSorted.length);
-        allColorsSorted.forEach(function (c) {
-            console.groupCollapsed("%c    %c " + c.key + " " + c.hexValue + " %c(" + c.value.count + " times)",
-                colorStyle(c.key), nameStyle, countStyle);
-            c.value.nodes.forEach(function (node) {
-                console.log(node);
-            });
-            console.groupEnd();
-        });
-        console.groupEnd("All colors used in elements on the page");
-    }
-
-    returnResult(allColorsSorted);
-
+  returnResult(allColorsSorted)
 }
 
-function returnResult(result) {
-    chrome.runtime.sendMessage({ result: result });
+function returnResult (result) {
+  chrome.runtime.sendMessage({ result: result })
 }
--- popup/script.js
+++ popup/script.js
@@ -1,70 +1,72 @@
-let options, result;
+/* global chrome, localStorage */
+let options
+let result
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.result) {
-        handleResult(request.result);
-    }
-});
+  if (request.result) {
+    handleResult(request.result)
+  }
+})
 
-function init() {
-    result = localStorage.getItem('allColors_result') ? JSON.parse(localStorage.getItem('allColors_result')) : false;
-    handleResult(result);
-    initialiseOptions();
+function init () {
+  result = localStorage.getItem('allColors_result') ? JSON.parse(localStorage.getItem('allColors_result')) : false
+  handleResult(result)
+  initialiseOptions()
 }
 
-function initialiseOptions() {
-    const defaultOptions = {
-        logToConsole: false,
-        includeBorderColorsWithZeroWidth: false,
-        props: ["background-color", "color", "border-top-color", "border-right-color", "border-bottom-color", "border-left-color"],
-        skipColors: { "rgb(0, 0, 0)": 1, "rgba(0, 0, 0, 0)": 1, "rgb(255, 255, 255)": 1 }
-    }
+function initialiseOptions () {
+  const defaultOptions = {
+    logToConsole: false,
+    includeBorderColorsWithZeroWidth: false,
+    props: ['background-color', 'color', 'border-top-color', 'border-right-color', 'border-bottom-color', 'border-left-color'],
+    skipColors: { 'rgb(0, 0, 0)': 1, 'rgba(0, 0, 0, 0)': 1, 'rgb(255, 255, 255)': 1 }
+  }
 
-    options = localStorage.getItem('allColors_options') ? JSON.parse(localStorage.getItem('allColors_options')) : defaultOptions;
+  options = localStorage.getItem('allColors_options') ? JSON.parse(localStorage.getItem('allColors_options')) : defaultOptions
 
-    document.getElementById('logToConsole').checked = options.logToConsole;
-    document.getElementById('includeZeroBorders').checked = options.includeBorderColorsWithZeroWidth;
-    options.props.forEach(function (property) {
-        addProperty(property);
-    });
-    Object.keys(options.skipColors).forEach(function (color) {
-        addSkipColor(color);
-    });
+  document.getElementById('logToConsole').checked = options.logToConsole
+  document.getElementById('includeZeroBorders').checked = options.includeBorderColorsWithZeroWidth
+  options.props.forEach(function (property) {
+    addProperty(property)
+  })
+  Object.keys(options.skipColors).forEach(function (color) {
+    addSkipColor(color)
+  })
 }
 
-function setOptions() {
-    let propsArry = Array.from(document.getElementById('props-list').getElementsByTagName('li'));
-    let skipColorsArry = Array.from(document.getElementById('skipColors-list').getElementsByTagName('li'));
-    let skipColors = new Object();
+function setOptions () {
+  let propsArry = Array.from(document.getElementById('props-list').getElementsByTagName('li'))
+  let skipColorsArry = Array.from(document.getElementById('skipColors-list').getElementsByTagName('li'))
+  let skipColors = {}
 
-    options.props = propsArry.map(function (prop) {
-        return prop.innerHTML;
-    });
+  options.props = propsArry.map(function (prop) {
+    return prop.innerHTML
+  })
 
-    skipColorsArry.forEach(function (element) {
-        skipColors[element.dataset.color] = 1;
-    });
+  skipColorsArry.forEach(function (element) {
+    skipColors[element.dataset.color] = 1
+  })
 
-    options.skipColors = skipColors;
-    options.includeBorderColorsWithZeroWidth = document.getElementById('includeZeroBorders').checked;
-    options.logToConsole = document.getElementById('logToConsole').checked;
-    localStorage.setItem('allColors_options', JSON.stringify(options));
+  options.skipColors = skipColors
+  options.includeBorderColorsWithZeroWidth = document.getElementById('includeZeroBorders').checked
+  options.logToConsole = document.getElementById('logToConsole').checked
+  localStorage.setItem('allColors_options', JSON.stringify(options))
 }
 
-function removeScript() {
-    const code = `
+function removeScript () {
+  const code = `
 		if (document.getElementById("allColorJS"))
         {
             document.getElementsByTagName("head")[0].removeChild(document.getElementById("allColorJS"));
         }
-	`;
+	`
 
-    chrome.tabs.executeScript({ code: code });
+  chrome.tabs.executeScript({ code: code })
 }
 
-function execAllColor() {
-    const file = `/all-colors/allcolors.js`;
-    const code = `
+function execAllColor () {
+  const file = `/all-colors/allcolors.js`
+  const code = `
         if (document.getElementById("allColorJS"))
         {
             //do nothing
@@ -76,128 +78,128 @@ function execAllColor() {
             allColorJS.id = "allColorJS";
             document.getElementsByTagName("head")[0].appendChild(allColorJS);
         }
-    `;
+    `
 
-    chrome.tabs.executeScript({ code: code });
+  chrome.tabs.executeScript({ code: code })
 }
 
-function handleResult(res) {
-    if (res) {
-        let container = document.getElementById("result-container");
-        
-        let label = document.createElement("label");
-        label.innerText = "Result";
+function handleResult (res) {
+  if (res) {
+    let container = document.getElementById('result-container')
 
-        let ul = document.createElement("ul");
-        ul.className = "result-list";
-        
-        container.appendChild(label);
+    let label = document.createElement('label')
+    label.innerText = 'Result'
 
-        res.forEach(function (c) {
-            let li = document.createElement("li");
-            let div = document.createElement("div");
-            let p = document.createElement("p");
+    let ul = document.createElement('ul')
+    ul.className = 'result-list'
 
-            div.className = "color-block";
-            div.style.backgroundColor = c.key;
-            p.innerHTML = c.key + " " + c.hexValue + " <b>(" + c.value.count + " Times)</b>";
+    container.appendChild(label)
 
-            li.appendChild(div);
-            li.appendChild(p);
-            ul.appendChild(li);
-        });
+    res.forEach(function (c) {
+      let li = document.createElement('li')
+      let div = document.createElement('div')
+      let p = document.createElement('p')
 
-        let li = document.createElement("li");
-        li.className = "total-colors";
-        li.innerText = "Total colors used: " + res.length;
-        
-        ul.appendChild(li);
-        container.appendChild(ul);
+      div.className = 'color-block'
+      div.style.backgroundColor = c.key
+      p.innerHTML = c.key + ' ' + c.hexValue + ' <b>(' + c.value.count + ' Times)</b>'
 
-        localStorage.setItem('allColors_result', JSON.stringify(res));
-    }
+      li.appendChild(div)
+      li.appendChild(p)
+      ul.appendChild(li)
+    })
+
+    let li = document.createElement('li')
+    li.className = 'total-colors'
+    li.innerText = 'Total colors used: ' + res.length
+
+    ul.appendChild(li)
+    container.appendChild(ul)
+
+    localStorage.setItem('allColors_result', JSON.stringify(res))
+  }
 }
 
-function addProperty(property) {
-    let ul = document.getElementById("props-list");
-    let li = document.createElement("li");
+function addProperty (property) {
+  let ul = document.getElementById('props-list')
+  let li = document.createElement('li')
 
-    li.onclick = function () {
-        this.parentElement.removeChild(this);
-    };
-    li.title = "Click to remove";
-    li.appendChild(document.createTextNode(property));
-    ul.appendChild(li);
+  li.onclick = function () {
+    this.parentElement.removeChild(this)
+  }
+  li.title = 'Click to remove'
+  li.appendChild(document.createTextNode(property))
+  ul.appendChild(li)
 }
 
-function addSkipColor(color) {
-    let ul = document.getElementById("skipColors-list");
-    let li = document.createElement("li");
-    let div = document.createElement("div");
+function addSkipColor (color) {
+  let ul = document.getElementById('skipColors-list')
+  let li = document.createElement('li')
+  let div = document.createElement('div')
 
-    li.onclick = function () {
-        this.parentElement.removeChild(this);
-    };
-    li.title = "Click to remove";
-    li.dataset.color = color;
-    div.className = "color-block";
-    div.style.backgroundColor = color;
+  li.onclick = function () {
+    this.parentElement.removeChild(this)
+  }
+  li.title = 'Click to remove'
+  li.dataset.color = color
+  div.className = 'color-block'
+  div.style.backgroundColor = color
 
-    li.appendChild(div);
-    li.appendChild(document.createTextNode(color));
-    ul.appendChild(li);
+  li.appendChild(div)
+  li.appendChild(document.createTextNode(color))
+  ul.appendChild(li)
 }
 
-function removeChildren(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    }
+function removeChildren (element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild)
+  }
 }
 
-function reset() {
-    let resultContainer = document.getElementById("result-container");
-    let skipColorsList = document.getElementById("skipColors-list");
-    let propsList = document.getElementById("props-list");
+function reset () {
+  let resultContainer = document.getElementById('result-container')
+  let skipColorsList = document.getElementById('skipColors-list')
+  let propsList = document.getElementById('props-list')
 
-    removeChildren(resultContainer);
-    removeChildren(skipColorsList);
-    removeChildren(propsList);
+  removeChildren(resultContainer)
+  removeChildren(skipColorsList)
+  removeChildren(propsList)
 
-    localStorage.removeItem('allColors_options');
-    localStorage.removeItem('allColors_result');
+  localStorage.removeItem('allColors_options')
+  localStorage.removeItem('allColors_result')
 
-    removeScript();
+  removeScript()
 }
 
 document.getElementById('trigger').addEventListener('click', function () {
-    setOptions();
-    let resultContainer = document.getElementById("result-container");
-    removeChildren(resultContainer);
+  setOptions()
+  let resultContainer = document.getElementById('result-container')
+  removeChildren(resultContainer)
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { settings: options }, function (response) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { settings: options }, function (response) {
 
-        });
-    });
+    })
+  })
 })
 
 document.getElementById('reset').addEventListener('click', function () {
-    reset();
-    init();
+  reset()
+  init()
 })
 
 document.getElementById('add-color').addEventListener('click', function () {
-    let colorInput = document.getElementById('skipColors');
-    addSkipColor(colorInput.value);
-    colorInput.value = "";
-    setOptions();
+  let colorInput = document.getElementById('skipColors')
+  addSkipColor(colorInput.value)
+  colorInput.value = ''
+  setOptions()
 })
 
 document.getElementById('add-property').addEventListener('click', function () {
-    let propsInput = document.getElementById('props');
-    addProperty(propsInput.value);
-    propsInput.value = "";
-    setOptions();
+  let propsInput = document.getElementById('props')
+  addProperty(propsInput.value)
+  propsInput.value = ''
+  setOptions()
 })
 
-init();
+init()
